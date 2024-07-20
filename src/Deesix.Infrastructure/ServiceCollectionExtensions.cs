d23e32bacd9bdee5b5cb479a4bbf63dd2873a561
Peter Milovcik <peter.milovcik@gmail.com>
2024-07-20 20:02:44 +0200

    Formatting

--- src/Deesix.Infrastructure/ServiceCollectionExtensions.cs
+++ src/Deesix.Infrastructure/ServiceCollectionExtensions.cs
@@ -12,25 +12,15 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddDeesixInfrastructure(this IServiceCollection services)
     {
         services.AddDeesixApplication();
+
         var basePath = AppDomain.CurrentDomain.BaseDirectory;
         var dbPath = Path.Combine(basePath, "database.db");        
         
         services.AddScoped<IRepository<Game>, GenericRepository<Game>>();
 
-        // // old
         services.AddDbContext<ApplicationDbContext>(options => 
             options.UseSqlite($"Data Source={dbPath}"));
-        // services.AddSingleton<IUserInterface, UserInterface>();
-        // services.AddSingleton<IGenerator, Generator>();
-        // services.AddSingleton<IWorldGenerator, WorldGenerator>();
-        // services.AddSingleton<IRealmGenerator, RealmGenerator>();
-        // services.AddSingleton<IRegionGenerator, RegionGenerator>();
-        // services.AddSingleton<ILocationGenerator, LocationGenerator>();
-        // services.AddSingleton<IOpenAIGenerator, OpenAIGenerator>();
-        // services.AddSingleton<IOpenAIApiKey, OpenAIApiKey>();
-        // services.AddScoped<IRepository<World>, GenericRepository<World>>();
-        // services.AddScoped<IRepository<Realm>, GenericRepository<Realm>>();
-        // services.AddSingleton<NewGame>();
+        
         return services;
     }
 }
