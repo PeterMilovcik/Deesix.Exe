37764cc035d4322d7d43affbf0b34c44871d6eba
Peter Milovcik <peter.milovcik@gmail.com>
2024-07-25 21:10:48 +0200

    Deesix.Tests.GameOptions.LoadGames.ExecuteAsync

---
+++ test/Deesix.Tests/GameOptions/LoadGames/ExecuteAsync.cs
@@ -0,0 +1,32 @@
+using Deesix.Application.GameOptions;
+using Deesix.Domain.Entities;
+using FluentAssertions;
+
+namespace Deesix.Tests.GameOptions.LoadGames;
+
+public class ExecuteAsync : GameOptionTestFixture<LoadGamesOption>
+{
+    public override void SetUp()
+    {
+        base.SetUp();
+        GameRepository.Add(new Game());
+    }
+
+    [Test]
+    public async Task Should_Not_Return_GameTurn_With_Empty_GameOptions() => 
+        (await GameOption!.ExecuteAsync(new GameTurn())).GameOptions
+            .Should().NotBeEmpty(because: "this game option should create " + 
+                "new specific game options for loading games.");
+    
+    [Test]
+    public async Task Should_Return_GameTurn_With_Correct_Message() => 
+        (await GameOption!.ExecuteAsync(new GameTurn())).Message
+            .Should().Be("Please choose a game to play.", 
+                because: "that is the expected next message.");
+    
+    [Test]
+    public async Task Should_Return_GameTurn_With_Correct_Question() => 
+        (await GameOption!.ExecuteAsync(new GameTurn())).Question
+            .Should().Be("Which one would you like to play?", 
+                because: "that is the expected next question.");
+}
