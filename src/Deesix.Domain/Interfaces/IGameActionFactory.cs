13239756201b698e8e8b6124e326b8c8f77f2693
Peter Milovcik <peter.milovcik@gmail.com>
2024-07-25 21:15:16 +0200

    Removed IGameAction, IGameActionFactory, GameActionFactory

--- src/Deesix.Domain/Interfaces/IGameActionFactory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.ObjectModel;
-using Deesix.Domain.Entities;
-
-namespace Deesix.Domain.Interfaces;
-
-public interface IGameActionFactory
-{
-    Collection<IGameAction> CreateActions(GameTurn gameTurn);
-}
