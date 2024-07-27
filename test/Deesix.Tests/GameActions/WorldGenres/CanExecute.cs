b6ed2ec58ebf194b99115e223e71a3ab8068cdf8
Peter Miľovčík <33028564+PeterMilovcik@users.noreply.github.com>
2024-07-27 19:28:20 +0000

    Deesix.Tests.GameActions.WorldGenres.ExecuteAsync

--- test/Deesix.Tests/GameActions/WorldGenres/CanExecute.cs
+++ test/Deesix.Tests/GameActions/WorldGenres/CanExecute.cs
@@ -6,6 +6,33 @@ using Moq;
 
 namespace Deesix.Tests.GameActions.WorldGenres;
 
+public class ExecuteAsync : GameActionTestFixture<WorldGenresGameOption>
+{
+    [Test]
+    public async Task Should_Return_GameTurn_With_Message() =>
+        (await GameAction!.ExecuteAsync(new GameTurn()))
+            .Message.Should().Be("Let's choose a specific world genre.",
+                because: "a message should be added to the GameTurn");
+
+    [Test]
+    public async Task Should_Return_GameTurn_With_Question() =>
+        (await GameAction!.ExecuteAsync(new GameTurn()))
+            .Question.Should().Be("Which one would you like to choose?",
+                because: "a question should be added to the GameTurn");
+    
+    [Test]
+    public async Task Should_Return_GameTurn_With_GameAction() =>
+        (await GameAction!.ExecuteAsync(new GameTurn()))
+            .GameActions.Should().NotBeEmpty(
+                because: "a new game action should be added to the GameTurn");
+    
+    [Test]
+    public async Task Should_Return_GameTurn_With_GameAction_With_Correct_Type() =>
+        (await GameAction!.ExecuteAsync(new GameTurn()))
+            .GameActions.First().Should().BeOfType<SpecificWorldGenreGameAction>(
+                because: "a new game action should be added to the GameTurn");
+}
+
 public class CanExecute : GameActionTestFixture<WorldGenresGameOption>
 {
     [Test]
