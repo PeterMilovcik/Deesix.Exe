8dac46e48f522200c347999aede46dd74a97b2a7
Peter Miľovčík <33028564+PeterMilovcik@users.noreply.github.com>
2024-07-27 19:10:21 +0000

    Deesix.Tests.GameActions.WorldGenres.Created

---
+++ test/Deesix.Tests/GameActions/WorldGenres/Created.cs
@@ -0,0 +1,18 @@
+﻿using Deesix.Application.GameActions;
+using Deesix.Tests.GameActions;
+using FluentAssertions;
+
+namespace Deesix.Tests.GameActions.WorldGenres;
+
+public class Created : GameActionTestFixture<WorldGenresGameOption>
+{
+    [Test]
+    public void Should_Have_Correct_Title() => 
+        GameAction!.Title.Should().Be("Choose a World Genre", 
+            because: "that is the expected title");
+    
+    [Test]
+    public void Should_Have_Correct_Order() =>
+        GameAction!.Order.Should().Be(1, 
+            because: "that is the expected order");
+}
