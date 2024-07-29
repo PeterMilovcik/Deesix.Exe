0aead367d6b19f470070e3c483973bfa3027d9a3
Peter Milovcik <peter.milovcik@gmail.com>
2024-07-29 20:41:21 +0200

    refactor: Rename IGameAction to IAction and update related classes and interfaces

---
+++ src/Deesix.Application/Actions/GenerateWorldSettings.cs
@@ -0,0 +1,21 @@
+﻿using Deesix.Domain.Entities;
+using Deesix.Domain.Interfaces;
+
+namespace Deesix.Application.Actions;
+
+public class GenerateWorldSettings : IAction
+{
+    public string Title => "Generate World Settings";
+
+    public int Order => 1;
+
+    public bool CanExecute(Turn turn)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Turn> ExecuteAsync(Turn turn)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ test/Deesix.Tests/Actions/GenerateWorldSettings/Created.cs
@@ -0,0 +1,9 @@
+﻿using Deesix.Application.Actions;
+using Deesix.Tests.Actions;
+
+namespace Deesix.Tests;
+
+public class Created : ActionTestFixture<GenerateWorldSettings>
+{
+
+}
